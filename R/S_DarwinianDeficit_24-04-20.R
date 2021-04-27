fed06ba0bc71129194d49a109d392fac780dc451
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-04-27 12:52:45 -0300

    mapping darwinian shortfall for all species

--- R/S_DarwinianDeficit_24-04-20.R
+++ R/S_DarwinianDeficit_24-04-20.R
@@ -1,12 +1,14 @@
 
 # libraries and data ------------------------------------------------------
-library(rgdal)     # R wrapper around GDAL/OGR
-library(ggplot2)   # for general plotting
-library(ggmap)    # for fortifying shapefiles
+library(rgdal)     
+library(ggplot2)  
+library(ggmap)   
 phylo_drainages <- readRDS(here::here("output", "phylo_drainages.rds"))
 shapefile <- rgdal::readOGR(here::here("data", "Basin042017_3119.shp"))
 
-# Next the shapefile has to be converted to a dataframe for use in ggplot2
+
+# formating shapefile for plot --------------------------------------------
+
 for(i in 1:length(shapefile@polygons)){
   shapefile@polygons[[i]]@ID <- shapefile@data$BasinName[i]
 }
@@ -14,18 +16,20 @@ shapefile_df <- fortify(shapefile)
 PD_dataframe <- data.frame(deficit = unlist(PD_deficit_all), id = names(PD_deficit_all))
 deficit_value <- unlist(lapply(shapefile_df$id, function(x) 
   PD_dataframe[which(x == PD_dataframe$id), "deficit"]
-  )
-  )
+)
+)
 data_map_PDeficit <- data.frame(shapefile_df, deficit = deficit_value)
 
-# Now the shapefile can be plotted as either a geom_path or a geom_polygon.
-# Paths handle clipping better. Polygons can be filled.
-# You need the aesthetics long, lat, and group.
+
+# plotting map ------------------------------------------------------------
+
 p <- ggplot(data = data_map_PDeficit) +
   geom_polygon(aes(x = long, y = lat, group = id, fill = deficit),
                colour = "black")
-quartz()
 Map_darwinian_shortfall <- p + scale_fill_viridis_c()
+
+
+# saving map --------------------------------------------------------------
 pdf("Map_darwinian.pdf", width = 15, height = 5)
 print(Map_darwinian_shortfall)
 dev.off()
