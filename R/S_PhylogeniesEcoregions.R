e55185749820ce2d68b462216a2114ef8e0d74cb
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2021-05-04 22:20:49 -0300

    change color tippoints and legend

--- R/S_PhylogeniesEcoregions.R
+++ R/S_PhylogeniesEcoregions.R
@@ -1,57 +1,94 @@
-# loading packages and data -----------------------------------------------
+# loading packages -----------------------------------------------
 library(ggtree)
 library(ggplot2)
 library(cowplot)
 
 
+# number of insertions ----------------------------------------------------
+percentage_insertion_afrotropics <- round(100*(length(unlist(lapply(c("Congeneric_insertion", "Congeneric_insertion_roundFamily", "Family_insertion", "Order_insertion"), function(x){
+  which(x == res_afrotropics$Insertions_data$insertions)
+})))/dim(res_afrotropics$Insertions_data)[1]), 1)
+
+percentage_insertion_neotropic <- round(100*(length(unlist(lapply(c("Congeneric_insertion", "Congeneric_insertion_roundFamily", "Family_insertion", "Order_insertion"), function(x){
+  which(x == res_neotropic$Insertions_data$insertions)
+})))/dim(res_neotropic$Insertions_data)[1]), 1)
+
+percentage_insertion_indomalay <- round(100*(length(unlist(lapply(c("Congeneric_insertion", "Congeneric_insertion_roundFamily", "Family_insertion", "Order_insertion"), function(x){
+  which(x == res_indomalay$Insertions_data$insertions)
+})))/dim(res_indomalay$Insertions_data)[1]), 1)
+
+percentage_insertion_neartic <- round(100*(length(unlist(lapply(c("Congeneric_insertion", "Congeneric_insertion_roundFamily", "Family_insertion", "Order_insertion"), function(x){
+  which(x == res_neartic$Insertions_data$insertions)
+})))/dim(res_neartic$Insertions_data)[1]), 1)
+
 # plotting trees and insertions -------------------------------------------
-cols <- c(Present_in_Tree = 'green', 
-          Congeneric_insertion = 'yellow', 
+cols <- c(Present_in_Tree = 'lightblue', 
+          Congeneric_insertion = 'yellow2', 
           Congeneric_insertion_roundFamily = 'orange', 
-          Family_insertion = "red")
+          Family_insertion = "red",
+          Order_insertion = "purple")
 
 p.base_afrotropics <- ggtree(res_afrotropics$Phylogeny, layout = "circular", size = .3)  %<+% res_afrotropics$Insertions_data +
   geom_treescale(x = 0, width = 20, linesize = .5, color = "blue", 
                  fontsize = 0) + #  plot the scale bar
   annotate("text", x = 4, y = 500, label = "20 myr", size = 1.5) # an attempt for add a scale bar
 phylo_afrotropics <- p.base_afrotropics + 
-  geom_tippoint(aes(color = insertions), size = .5) +
-  scale_color_manual(values = cols)
+  geom_tippoint(aes(color = insertions), size = .5, alpha = .8) +
+  scale_color_manual(values = cols, name = "Insertions", labels = c("Congeneric", 
+                                                                    "Congeneric in Family",
+                                                                    "Family", 
+                                                                    "Order",
+                                                                    "Present in tree")
+  )
 p.base_indomalay <- ggtree(res_indomalay$Phylogeny, layout = "circular", size = .3)  %<+% res_indomalay$Insertions_data +
   geom_treescale(x = 0, width = 20, linesize = .5, color = "blue", 
                  fontsize = 0) + #  plot the scale bar
   annotate("text", x = 4, y = 500, label = "20 myr", size = 1.5) # an attempt for add a scale bar
 phylo_indomalay <- p.base_indomalay + 
-  geom_tippoint(aes(color = insertions), size = .5) +
-  scale_color_manual(values = cols)
+  geom_tippoint(aes(color = insertions), size = .5, alpha = .8) +
+  scale_color_manual(values = cols, name = "Insertions", labels = c("Congeneric", 
+                                                                    "Congeneric in Family",
+                                                                    "Family", 
+                                                                    "Order",
+                                                                    "Present in tree")
+  )
 p.base_neartic <- ggtree(res_neartic$Phylogeny, layout = "circular", size = .3)  %<+% res_neartic$Insertions_data +
   geom_treescale(x = 0, width = 20, linesize = .5, color = "blue", 
                  fontsize = 0) + #  plot the scale bar
   annotate("text", x = 4, y = 500, label = "20 myr", size = 1.5) # an attempt for add a scale bar
 phylo_neartic <- p.base_neartic + 
-  geom_tippoint(aes(color = insertions), size = .5) +
-  scale_color_manual(values = cols)
+  geom_tippoint(aes(color = insertions), size = .5, alpha = .8) +
+  scale_color_manual(values = cols, name = "Insertions", labels = c("Congeneric", 
+                                                                    "Congeneric in Family",
+                                                                    "Family", 
+                                                                    "Order",
+                                                                    "Present in tree")
+                     )
 p.base_neotropic <- ggtree(res_neotropic$Phylogeny, layout = "circular", size = .3)  %<+% res_neotropic$Insertions_data +
   geom_treescale(x = 0, width = 20, linesize = .5, color = "blue", 
                  fontsize = 0) + #  plot the scale bar
   annotate("text", x = 4, y = 500, label = "20 myr", size = 1.5) # an attempt for add a scale bar
 phylo_neotropic <- p.base_neotropic + 
-  geom_tippoint(aes(color = insertions), size = .5) +
-  scale_color_manual(values = cols)
+  geom_tippoint(aes(color = insertions), size = .5, alpha = .8) +
+  scale_color_manual(values = cols, name = "Insertions", labels = c("Congeneric", 
+                                                                    "Congeneric in Family",
+                                                                    "Family", 
+                                                                    "Order",
+                                                                    "Present in tree"))
 
 pgrid <- plot_grid(
   phylo_afrotropics + theme(legend.position="none"),
   phylo_indomalay + theme(legend.position="none"),
   phylo_neartic + theme(legend.position="none"),
   phylo_neotropic + theme(legend.position="none"), 
-  labels = c('Afrotropic', 'Indo-Malay', "Neartic", "Neotropic"),
+  labels = c(paste('Afrotropic', paste(percentage_insertion_afrotropics, "%", sep = ""), sep = " "),
+             paste('Indo-Malay', paste(percentage_insertion_indomalay, "%", sep = ""), sep = " "),
+             paste("Neartic", paste(percentage_insertion_neartic, "%", sep = ""), sep = " "),
+             paste("Neotropic", paste(percentage_insertion_neotropic, "%", sep = ""), sep = " ")),
   nrow = 2
 )
 
 # extract the legend from one of the plots
-legend <- get_legend(
-  phylo_afrotropics, theme(legend.box.margin = margin(0, 0, 0, 12))
-)
 legend <- get_legend(
   phylo_afrotropics + 
     guides(color = guide_legend(nrow = 1)) +
