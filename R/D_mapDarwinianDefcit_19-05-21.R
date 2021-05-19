73fb1947a9f2b9f367e74c3712a87fbfae59918f
Aline Richter <linebio.r@gmail.com>
2021-05-19 19:44:40 -0300

    minor changes

--- R/D_mapDarwinianDefcit_19-05-21.R
+++ R/D_mapDarwinianDefcit_19-05-21.R
@@ -1,7 +1,7 @@
 
 # reading data and functions ----------------------------------------------
 # data
-osm_raw_data <- read.csv(here::here("data", "osm-raw-data.csv"), sep = ";")
+# osm_raw_data <- read.csv(here::here("data", "osm-raw-data.csv"), sep = ";")
 drainage_basins <- read.csv(here::here("data", "Drainage_Basins_Table.csv"), sep = ";")
 occ_drainage <- read.csv(here::here("data", "Occurrence_Table.csv"), sep = ";")
 occ_drainage$X2.Species.Name.in.Source <- gsub("[.]", "_", occ_drainage$X2.Species.Name.in.Source)
@@ -34,13 +34,22 @@ FishPhyloMaker_data <- taxon_data$Taxon_data_FishPhyloMaker
 FishPhyloMaker_data <- FishPhyloMaker_data[- which(is.na(unique(FishPhyloMaker_data$s)) == TRUE), ]
 FishPhyloMaker_data[which(FishPhyloMaker_data$f == "Cichlidae"), "o"] <- "Cichliformes"
 FishPhyloMaker_data[which(FishPhyloMaker_data$o == "Scorpaeniformes"), "o"] <- "Perciformes"
-
+memory.limit(99999)
 system.time(phylo_drainages <- FishPhyloMaker(data = FishPhyloMaker_data,
                                               insert.base.node = TRUE,
                                               return.insertions = TRUE,
                                               progress.bar = TRUE))
 saveRDS(object = phylo_drainages, file = here::here("output", "phylo_drainages.rds"))
 
+insertions_allDrainages <- phylo_drainages$Insertions_data
+phylo_allDrainages <- phylo_drainages$Phylogeny
+basin_names <- unique(occ_drainage$X1.Basin.Name)
+list_spp_perBasin <- lapply(basin_names, function(x) occ_drainage[which(x == occ_drainage$X1.Basin.Name), "X6.Fishbase.Valid.Species.Name"])
+names(list_spp_perBasin) <- unique(occ_drainage$X1.Basin.Name)
+phy_perBasin <- lapply(list_spp_perBasin, function(x){
+  rm_tip <- ape::drop.tip(phylo_drainages$Phylogeny, tip = x)$tip.label
+  ape::drop.tip(phylo_drainages$Phylogeny, tip = rm_tip)
+})
 
 PD_deficit_all <- lapply(phy_perBasin, function(x) PD_defict(phylo = x, 
                                                              data = phylo_drainages$Insertions_data, 
