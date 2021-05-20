a82632b27aeb115b2ac3d5c04258838879cc8535
Aline Richter <linebio.r@gmail.com>
2021-05-20 15:28:16 -0300

    proofreading

--- R/functions/F_darwinianDeficit.R
+++ R/functions/F_darwinianDeficit.R
@@ -1,10 +1,10 @@
-PD_defict <- function(phylo, data, level = "Congeneric_insertion"){
+PD_deficit <- function(phylo, data, level = "Congeneric_insertion"){
   names_exclude <- phylo$tip.label[na.omit(match(data[which(data$insertions == "Present_in_Tree"), "s"], 
                                 phylo$tip.label))]
   
   if(all(!is.na(match( phylo$tip.label, names_exclude))) == TRUE){
     PD_present <- sum(phylo$edge.length)
-    Darwinian_defict <- 0
+    Darwinian_deficit <- 0
   } else{
     exclude <- ape::drop.tip(phylo, tip = names_exclude)$tip.label
     phylo_present <- ape::drop.tip(phylo, tip = exclude)
@@ -19,7 +19,7 @@ PD_defict <- function(phylo, data, level = "Congeneric_insertion"){
     phylo_level <- ape::drop.tip(phylo, level_exclude)
     PD_level <- sum(phylo_level$edge.length)
     PD_total <- sum(PD_present, PD_level)
-    Darwinian_defict <- PD_level/PD_total
+    Darwinian_deficit <- PD_level/PD_total
   }
-  return(Darwinian_defict)
+  return(Darwinian_deficit)
 }
