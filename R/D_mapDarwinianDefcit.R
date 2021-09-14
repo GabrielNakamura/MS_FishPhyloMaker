50278bdc85e2062a5cb14cf826faf186baa937a0
SeuNomeDeUsuario <seuMail@example.br>
2021-09-14 14:10:34 -0300

    removing old calculations of shortfall

---
+++ R/D_mapDarwinianDefcit.R
@@ -0,0 +1,134 @@
+
+# reading data and functions ----------------------------------------------
+# data
+# osm_raw_data <- read.csv(here::here("data", "osm-raw-data.csv"), sep = ";")
+drainage_basins <- read.csv(here::here("data", "Drainage_Basins_Table.csv"), sep = ";")
+occ_drainage <- read.csv(here::here("data", "Occurrence_Table.csv"), sep = ";")
+occ_drainage$X2.Species.Name.in.Source <- gsub("[.]", "_", occ_drainage$X2.Species.Name.in.Source)
+occ_drainage$X6.Fishbase.Valid.Species.Name <- gsub("[.]", "_", occ_drainage$X6.Fishbase.Valid.Species.Name)
+
+
+# libraries
+library(FishPhyloMaker)
+
+names_spp_valid <- unique(occ_drainage$X6.Fishbase.Valid.Species.Name)
+
+# obtaining phylogeny  ----------------------------------------------------
+taxon_data <- FishPhyloMaker::FishTaxaMaker(data = names_spp_valid, allow.manual.insert = TRUE)
+Pimelodidae
+Siluriformes 
+Petromyzontidae
+Petromyzontiformes
+Nemacheilidae
+Cypriniformes
+Trichomycteridae
+Siluriformes
+Procatopodidae
+Cyprinodontiformes
+
+
+# correcting families and orders ------------------------------------------
+
+FishPhyloMaker_data <- taxon_data$Taxon_data_FishPhyloMaker
+FishPhyloMaker_data <- FishPhyloMaker_data[- which(is.na(unique(FishPhyloMaker_data$s)) == TRUE), ]
+FishPhyloMaker_data[which(FishPhyloMaker_data$f == "Cichlidae"), "o"] <- "Cichliformes"
+FishPhyloMaker_data[which(FishPhyloMaker_data$o == "Scorpaeniformes"), "o"] <- "Perciformes"
+
+
+# making phylogeny --------------------------------------------------------
+
+phylo_drainages <- FishPhyloMaker(data = FishPhyloMaker_data,
+                                              insert.base.node = TRUE,
+                                              return.insertions = TRUE,
+                                              progress.bar = TRUE)
+
+insertions_allDrainages <- phylo_drainages$Insertions_data
+
+phylo_allDrainages <- phylo_drainages$Phylogeny
+
+basin_names <- unique(occ_drainage$X1.Basin.Name)
+
+list_spp_perBasin <- lapply(basin_names, function(x) occ_drainage[which(x == occ_drainage$X1.Basin.Name), "X6.Fishbase.Valid.Species.Name"])
+
+names(list_spp_perBasin) <- unique(occ_drainage$X1.Basin.Name)
+
+phy_perBasin <- lapply(list_spp_perBasin, function(x){
+  rm_tip <- ape::drop.tip(phylo_drainages$Phylogeny, tip = x)$tip.label
+  ape::drop.tip(phylo_drainages$Phylogeny, tip = rm_tip)
+})
+
+PD_deficit_all <- lapply(phy_perBasin, function(x) Darwinian_deficit(phylo = x, 
+                                                             data = phylo_drainages$Insertions_data, 
+                                                             level = "Congeneric_insertion")
+)
+
+# deficit by orders -------------------------------------------------------
+
+phy_perBasin <- lapply(list_spp_perBasin, function(x){
+  rm_tip <- ape::drop.tip(phylo_drainages$Phylogeny, tip = x)$tip.label
+  ape::drop.tip(phylo_drainages$Phylogeny, tip = rm_tip)
+})
+
+phy_perBasin_rmphy <- phy_perBasin[-match(names(unlist(lapply(phy_perBasin, function(x) which(length(x) == 0)))), 
+                                          names(phy_perBasin))]
+## siluriformes
+
+phy_perBasin_Siluri <- lapply(phy_perBasin_rmphy, function(x) ape::drop.tip(x, 
+                                                                            ape::drop.tip(x, 
+                                                                                          tip = dplyr::filter(phylo_drainages$Insertions_data, 
+                                                                                                              o == "Siluriformes")[, "s"])$tip.label)
+                              )
+PD_deficit_Siluri <- unlist(lapply(phy_perBasin_Siluri, function(x) FishPhyloMaker::PD_defict(phylo = x, 
+                                                                     data = phylo_drainages$Insertions_data, 
+                                                                     level = "Congeneric_insertion")
+))
+
+## Characiformes
+
+phy_perBasin_Characi <- lapply(phy_perBasin_rmphy, function(x) ape::drop.tip(x, 
+                                                                            ape::drop.tip(x, 
+                                                                                          tip = dplyr::filter(phylo_drainages$Insertions_data, 
+                                                                                                              o == "Characiformes")[, "s"])$tip.label)
+)
+PD_deficit_Characi <- unlist(lapply(phy_perBasin_Characi, function(x) FishPhyloMaker::PD_defict(phylo = x, 
+                                                                                              data = phylo_drainages$Insertions_data, 
+                                                                                              level = "Congeneric_insertion")
+))
+
+## Cipryniformes
+
+phy_perBasin_Cyprini <- lapply(phy_perBasin_rmphy, function(x) ape::drop.tip(x, 
+                                                                             ape::drop.tip(x, 
+                                                                                           tip = dplyr::filter(phylo_drainages$Insertions_data, 
+                                                                                                               o == "Cypriniformes")[, "s"])$tip.label)
+)
+PD_deficit_Cyprini <- unlist(lapply(phy_perBasin_Cyprini, function(x) FishPhyloMaker::PD_defict(phylo = x, 
+                                                                                               data = phylo_drainages$Insertions_data, 
+                                                                                               level = "Congeneric_insertion")
+))
+
+## Perciformes
+
+phy_perBasin_Perci <- lapply(phy_perBasin_rmphy, function(x) ape::drop.tip(x, 
+                                                                             ape::drop.tip(x, 
+                                                                                           tip = dplyr::filter(phylo_drainages$Insertions_data, 
+                                                                                                               o == "Perciformes")[, "s"])$tip.label)
+)
+PD_deficit_Perci <- unlist(lapply(phy_perBasin_Perci, function(x) FishPhyloMaker::PD_defict(phylo = x, 
+                                                                                               data = phylo_drainages$Insertions_data, 
+                                                                                               level = "Congeneric_insertion")
+))
+
+## data frame deficit per order
+
+PD_deficit_perOrder <- data.frame(deficitChar = PD_deficit_Characi,
+                               deficitPerci = PD_deficit_Perci, 
+                               deficitCypri = PD_deficit_Cyprini, 
+                               deficitSilu = PD_deficit_Siluri)
+
+# saving objects ----------------------------------------------------------
+
+saveRDS(object = phylo_drainages, file = here::here("output", "phylo_drainages.rds"))
+saveRDS(object = PD_deficit_perOrder, file = here::here("output", "PD_deficit_perOrder.rds"))
+saveRDS(PD_deficit_all, file = here::here("output", "PD_deficit_all.rds"))
+
