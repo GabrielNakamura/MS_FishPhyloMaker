f4fb62148905fd6f7d8a2c94d1946efcdae8ce51
SeuNomeDeUsuario <seuMail@example.br>
2021-09-29 07:21:44 -0300

    phylogenies with new package version

---
+++ R/D_makingPhylo_all_Basins.R
@@ -0,0 +1,42 @@
+
+# read libraries ---------------------------------------------------------------
+
+library(FishPhyloMaker)
+
+# analyzing all data ------------------------------------------------------
+
+species_list <- unique(occ_drainage$X6.Fishbase.Valid.Species.Name)
+
+all_taxa_names <- FishTaxaMaker(data = species_list, allow.manual.insert = TRUE)
+Cyprinidae
+Loricariidae
+Pseudopimelodidae
+Petromyzontidae
+Nemacheilidae
+Loricariidae
+Trichomycteridae
+Procatopodidae
+
+phylo_all_spp <- FishPhyloMaker(data = all_taxa_names$Taxon_data_FishPhyloMaker, 
+                                insert.base.node = TRUE, 
+                                return.insertions = TRUE)
+
+phylo_all <- phylo_all_spp$Phylogeny
+
+
+# separating ecoregions ---------------------------------------------------
+
+ecoregion <- names(table(data$X3_Ecoregion))
+list_ecoregions <- lapply(ecoregion, function(x) data[which(data$X3_Ecoregion == x), ])
+names(list_ecoregions) <- ecoregion
+species_list_ecoregions <- lapply(list_ecoregions, function(x) x$Genus.species)
+names(species_list_ecoregions) <- ecoregion
+
+
+# phylogenies per ecoregion ------------------------------------------------------
+
+
+# saving results ----------------------------------------------------------
+saveRDS(object = phylo_all, file = here::here("output", "phylo_all.rds"))
+
+
