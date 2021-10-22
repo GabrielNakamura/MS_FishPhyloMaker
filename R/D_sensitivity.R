33c96bcf04f8ae6365521533e10ad9d80e4d013f
SeuNomeDeUsuario <seuMail@example.br>
2021-10-22 18:32:18 -0300

    excluding percentage of insertions

--- R/D_sensitivity.R
+++ R/D_sensitivity.R
@@ -7,14 +7,6 @@ library(rfishbase)
 library(parallel)
 source(here::here("R", "functions", "function_evalPhyloMaker.R"))
 
-# percentage of insertions per ecoregion ----------------------------------
-
-unlist(lapply(lapply(lapply(unique(data_basin_ecoregion$Ecoregion), 
-                            function(x) which(data_basin_ecoregion$Ecoregion == x &
-                                                data_basin_ecoregion$insertions != "Present_in_Tree")), function(y){
-                                                  unique(data_basin_ecoregion[y,"spp"])
-                                                }), function(z) length(z)/length(phylo_complete$tip.label)))
-
 
 # sensitivity analysis ----------------------------------------------------
 
