b6c11a9a63b189e77efe5998a8724d13e55f0e79
SeuNomeDeUsuario <seuMail@example.br>
2021-10-22 18:31:17 -0300

    saving result objects

--- R/D_sensitivity.R
+++ R/D_sensitivity.R
@@ -25,13 +25,21 @@ res_01 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
                     insert.base.node = TRUE, 
                     return.insertions = TRUE, 
                     progress.bar = TRUE)
-parallel::stopCluster(parallel)
+res_015 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
+                              probs = 0.15, 
+                              insert.base.node = TRUE, 
+                              return.insertions = TRUE, 
+                              progress.bar = TRUE)
 res_02 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
                               probs = 0.2, 
                               insert.base.node = TRUE, 
                               return.insertions = TRUE, 
                               progress.bar = TRUE)
-parallel::stopCluster(parallel)
+res_025 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
+                              probs = 0.25, 
+                              insert.base.node = TRUE, 
+                              return.insertions = TRUE, 
+                              progress.bar = TRUE)
 res_03 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
                               probs = 0.3, 
                               insert.base.node = TRUE, 
@@ -39,4 +47,8 @@ res_03 <- parallel::parLapply(parallel, 1:50, fun = eval_PhyloMaker,
                               progress.bar = TRUE)
 parallel::stopCluster(parallel)
 
-saveRDS(object = res_01, file = here::here("output", "res_010_20runs.rds"))
+saveRDS(object = res_01, file = here::here("output", "res_010_50runs.rds"))
+saveRDS(object = res_015, file = here::here("output", "res_015_50runs.rds"))
+saveRDS(object = res_020, file = here::here("output", "res_020_50runs.rds"))
+saveRDS(object = res_025, file = here::here("output", "res_025_50runs.rds"))
+saveRDS(object = res_030, file = here::here("output", "res_030_50runs.rds"))
