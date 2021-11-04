5f9f37efadd684b4c870c69604b926d0638b62cb
SeuNomeDeUsuario <seuMail@example.br>
2021-11-04 09:43:18 -0300

    updating results for sensitivity analysis

--- R/S_sensitivity.R
+++ R/S_sensitivity.R
@@ -19,10 +19,10 @@ distinctness <- ggplot(data = df_sensitivity,
   geom_boxplot() +
   geom_jitter(color="gray", size=0.6, alpha=0.6) +
   scale_x_discrete(labels= insertionPercentagelab) +
-  scale_y_continuous(name = "Correlation coefficient",
-                     limits = c(0, 1)) +
-  ggtitle("A") +
-  xlab("Percentage of inserted species (%)") +
+  scale_y_continuous(limits = c(.4, 1)) +
+  labs(x = "Percentage of inserted species (%)", 
+       y = "Correlation coefficient", 
+       tag = "A") +
   theme(legend.position = "", panel.background = element_rect(fill = "transparent"),
         axis.line = element_line(colour = "black", size = 0.8, linetype = "solid"),
         plot.margin = unit(c(0.4, 0.4, 0.4, 0.4), "mm"),
@@ -39,30 +39,34 @@ distinctness <- ggplot(data = df_sensitivity,
 
 
 coph <- ggplot(data = df_sensitivity, 
-               aes(x = percent_insert, y = cor_cophenetic, fill = cor_cophenetic)) +
+               aes(x = percent_insert, y = cor_cophenetic)) +
   geom_boxplot() +
   geom_jitter(color="gray", size=0.8, alpha=0.6) +
   scale_x_discrete(labels= insertionPercentagelab) +
-  xlab("Percentage of inserted species (%)") +
-  ylab("Correlation coefficient") +
-  ggtitle("B") +
+  labs(x = "Percentage of inserted species (%)",
+       y = "Correlation coefficient",
+       tag = "B") +
   theme(legend.position = "", panel.background = element_rect(fill = "transparent"),
         axis.line = element_line(colour = "black", size = 0.8, linetype = "solid"),
         plot.margin = unit(c(0.4, 0.4, 0.4, 0.4), "mm"),
         legend.title = element_text(family = "Times", color = "black", face = "bold", size = 12),
         legend.text = element_text(family = "Times", color = "black", size = 12), 
-        axis.text = element_text(family = "Times", color = "black", size = 12), panel.border = element_blank(), axis.ticks = element_blank(),
+        axis.text = element_text(family = "Times", color = "black", size = 12), 
+        panel.border = element_blank(), axis.ticks = element_blank(),
         plot.subtitle = element_text(family = "Arial", 
                                      color = "black",
                                      size = 9, 
                                      hjust = 0.5, 
                                      margin = margin(b = 6)
         )
-  ) + facet_zoom(ylim = c(0, 1))
+  )
 
-sensitivity_plot <- cowplot::plot_grid(distinctness, coph)
+sensitivity_plot <- cowplot::plot_grid(distinctness, coph, nrow = 2)
 
+
+
+# saving graphic ----------------------------------------------------------
 
 ggsave(filename = here::here("output", "images", "sensitivity_plot.png"), plot = sensitivity_plot, 
-       width = 15, height = 7,
+       width = 7, height = 9,
        dpi = 500)
